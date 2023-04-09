2c6155200c09d6cebb85264751c59a9ea5629f06
dk3156 <78196670+dk3156@users.noreply.github.com>
2023-04-08 21:09:42 -0400

    update1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 # load data
 model_list = ["distilbert-base-uncased-finetuned-sst-2-english", "cardiffnlp/twitter-roberta-base-sentiment", "finiteautomata/bertweet-base-sentiment-analysis"]
 model_name = st.selectbox('Which model to predict?', model_list)
-text = st.text_input("text for sentiment analysis", value="I love you")
+text = st.text_input("text for sentiment analysis", value="This is gonna be good")
 
 # run to get result
 if st.button('Run'):
