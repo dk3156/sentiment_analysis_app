769ee3fe141c33fd12e1ee84fd9906d776a817d3
dk3156 <78196670+dk3156@users.noreply.github.com>
2023-04-08 21:46:23 -0400

    update6

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ model_name = st.selectbox('Which model would you like to use?', name_dict.keys()
 text = st.text_input("Enter your text!", value="Oh wow, I did not know that.")
 
 if st.button('Analyze'):
-    classifier = pipeline(model=name_dict[model_name])
+    classifier = pipeline(model=name_dict[model_name], return_all_scores=True)
     result = classifier(text)
     st.write(result)
