00678e890a567b8df89aabe4d18faca882d46c8f
dk3156 <78196670+dk3156@users.noreply.github.com>
2023-04-08 21:19:29 -0400

    update2

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import streamlit as st
 from transformers import pipeline
 import pandas as pd
 
-# load data
-model_list = ["distilbert-base-uncased-finetuned-sst-2-english", "cardiffnlp/twitter-roberta-base-sentiment", "finiteautomata/bertweet-base-sentiment-analysis"]
-model_name = st.selectbox('Which model to predict?', model_list)
-text = st.text_input("text for sentiment analysis", value="This is gonna be good")
+name_list = ["distilbert-base-uncased-finetuned-sst-2-english", "cardiffnlp/twitter-roberta-base-sentiment", "cardiffnlp/twitter-xlm-roberta-base-sentiment"]
+model_name = st.selectbox('Which model would you like to use?', name_list)
+text = st.text_input("Please input text to analyze", value="This is gonna be good")
 
-# run to get result
 if st.button('Run'):
     sentiment_pipeline = pipeline(model=model_name)
     result = sentiment_pipeline(text)
