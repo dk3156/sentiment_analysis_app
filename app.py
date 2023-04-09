7f382d49b65e8206170ef5e5090065c83efb4672
dk3156 <78196670+dk3156@users.noreply.github.com>
2023-04-08 21:34:49 -0400

    update5

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from transformers import pipeline
 
-name_list = ["distilbert-base-uncased-finetuned-sst-2-english", "j-hartmann/emotion-english-distilroberta-base"]
-model_name = st.selectbox('Which model would you like to use?', name_list)
-text = st.text_input("Input text to be analyzed", value="Oh wow, I did not know that.")
+name_dict = {"Binary" : "distilbert-base-uncased-finetuned-sst-2-english", "Non-binary" : "j-hartmann/emotion-english-distilroberta-base"}
+model_name = st.selectbox('Which model would you like to use?', name_dict.keys())
+text = st.text_input("Enter your text!", value="Oh wow, I did not know that.")
 
-if st.button('Run'):
-    classifier = pipeline(model=model_name)
+if st.button('Analyze'):
+    classifier = pipeline(model=name_dict[model_name])
     result = classifier(text)
     st.write(result)
