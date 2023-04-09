59c29d313efa03e32d38d0c7d674657ba4861936
dk3156 <78196670+dk3156@users.noreply.github.com>
2023-04-08 21:25:55 -0400

    update4

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 
 name_list = ["distilbert-base-uncased-finetuned-sst-2-english", "j-hartmann/emotion-english-distilroberta-base"]
 model_name = st.selectbox('Which model would you like to use?', name_list)
-text = st.text_input("Please input text to analyze", value="Oh wow, I did not know that.")
+text = st.text_input("Input text to be analyzed", value="Oh wow, I did not know that.")
 
 if st.button('Run'):
     classifier = pipeline(model=model_name)
