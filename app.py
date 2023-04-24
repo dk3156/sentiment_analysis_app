fe2d5cc8709063c36ecf9e805251f14986b76429
dk3156 <78196670+dk3156@users.noreply.github.com>
2023-04-23 21:10:05 -0400

    commit again

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 import streamlit as st
-from transformers import pipeline
-
-name_dict = {"Binary" : "distilbert-base-uncased-finetuned-sst-2-english", "Non-binary" : "j-hartmann/emotion-english-distilroberta-base"}
-model_name = st.selectbox('Which model would you like to use?', name_dict.keys())
-text = st.text_input("Enter your text!", value="Oh wow, I did not know that.")
-
-if st.button('Analyze'):
-    classifier = pipeline(model=name_dict[model_name], return_all_scores=True)
-    result = classifier(text)
-    st.write(result)
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import emoji
+import demoji
 
