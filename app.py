eb28eeb2bfbd41b88fd87b8b26fa36d5d99c60d5
Dongje Kim <dk3156@nyu.edu>
2023-04-24 17:17:21 -0400

    final

--- app.py
+++ app.py
@@ -2,14 +2,28 @@ import streamlit as st
 import pandas as pd
 from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification, pipeline
 import pandas as pd
-import re
 import torch
 
 labels= ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 
-# table = {
-#     "Why the edits made under my usern..." : 
-# }
+first_ten_tweets = {
+    "Yo bitch Ja Rule is more succe..." : 0.9988,
+    "The title is..." : 0.0011,
+    "Zawe A..." : 0.0027,
+    "If you have a look back at th..." : 0.0005,
+    "I don't anonymously edit artic..." : 0.0008,
+    "Thank you for understanding..." : 0.0005,
+    "Please do not add nonsense to..." : 0.0008,
+    "Dear god this site is horribl..." : 0.8693,
+    "Only a fool can believe in..." : 0.1242,
+    "When..." : 0.0012,
+}
+
+st.title("Sentiment Analysis")
+
+st.title("First Ten Tweets")
+for elem in first_ten_tweets:
+    st.write("Tweet: ", elem[0], " with toxicity of", elem[1])
 
 def finetune(text):
     model = "dk3156/toxic_tweets_model"
@@ -19,18 +33,11 @@ def finetune(text):
     res = finetune(**tokens)
     scores = torch.sigmoid(res.logits)
     output = ""
-    print(text[:30], scores[0][0].item())
-
-    # for i in range(len(labels)):
-    #     output += labels[i] + ": " + str(round(scores[0][i].item(), 1)) + " "
-    # st.write(text[:20] + "... \n" + output)
+    for i in range(len(labels)):
+        output += labels[i] + ": " + str(round(scores[0][i].item(), 4)) + " "
+    st.write(text[:20] + "... \n" + output)
 
 df_test = pd.read_csv('./sample_data/test.csv')
-
-for text in df_test['comment_text'][:10]:
-    finetune(text)
-
-st.title("Sentiment Analysis")
 
 text = st.text_input("Enter your text", value="Your dress looks like one colorful dishcloth!")
 
